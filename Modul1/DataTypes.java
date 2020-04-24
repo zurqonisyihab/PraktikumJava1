c57b65f8c70e39671785be22e954ebd879dffce4
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-24 02:10:03 -0700

    Output dengan sout

--- Modul1/DataTypes.java
+++ Modul1/DataTypes.java
@@ -9,6 +9,15 @@ puclic class DataTypes{
   double ipk = 3.8976512;
   char nilaiAbjad = 'A';
   boolean tampan = true;
+  
+  System.out.println("Nama depan : " + namaDepan);
+  System.out.println("Nama tengah : " + namaTengah);
+  System.out.println("Nama belakang " + namaBelakang);
+  System.out.println("Usia : " + usia);
+  System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+  System.out.println("IPK :" + ipk);
+  System.out.println("Nilai PBO : " + nilaiAbjad);
+  System.out.println("Tampan : " + tampan);
 
  }
 }
