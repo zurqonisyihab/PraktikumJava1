24733841073465a0700be191b1b40f2e468579ce
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-24 02:35:55 -0700

    Input dengan Scanner

--- Modul1/DataTypes.java
+++ Modul1/DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args){
+
+  Scanner scanner = new Scanner(System.in);
 
   String namaDepan = "Muhammad";
   String namaTengah = "Zurqoni";
@@ -11,7 +14,21 @@ public class DataTypes{
   double ipk = 3.8976512;
   char nilaiAbjad = 'A';
   boolean tampan = true;
-  
+
+  System.out,print("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out,print("Input Usia : ");
+  usia = scanner.nextInt();
+  System.out,print("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out,print("");
+  System.out,print("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out,print("Tampan? : ");
+  tampan = scanner.nextBoolean();
+
+
+  System.out,println("========= OUTPUT ==========");
   System.out.println("Nama depan : " + namaDepan);
   System.out.println("Nama tengah : " + namaTengah);
   System.out.println("Nama belakang " + namaBelakang);
