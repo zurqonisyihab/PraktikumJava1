076d5f85db801a63a5e11d2718b7db1a19189dbc
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-24 02:01:13 -0700

    deklarasi variabel dan tipe data

---
+++ Modul1/DataTypes.java
@@ -0,0 +1,14 @@
+puclic class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Muhammad";
+  String namaTengah = "Zurqoni";
+  String namaBelakang = "Sihab";
+  int usia = 18
+  int targetTahunKuliah = 4;
+  double ipk = 3.8976512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+
+ }
+}
